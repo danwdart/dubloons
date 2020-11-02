34b7a46c39e3338d7692dae8b3969999298e5e22
Dan Dart <git@dandart.co.uk>
2020-11-02 00:50:50 +0000

    maybe something nix related

--- default.nix
+++ default.nix
@@ -1,19 +1,2 @@
-{ mkDerivation, aeson, base, bytestring, containers
-, discord-haskell, feed, http-client, hxt, hxt-xpath, mtl, process
-, req, retry, safe-foldable, stdenv, text, transformers, unix
-, xml-types
-}:
-mkDerivation {
-  pname = "dubloons";
-  version = "0.6.0.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
-    aeson base bytestring containers discord-haskell feed http-client
-    hxt hxt-xpath mtl process req retry safe-foldable text transformers
-    unix xml-types
-  ];
-  homepage = "https://github.com/danwdart/dubloons#readme";
-  license = stdenv.lib.licenses.agpl3;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8102" }:
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./dubloons.nix { }
