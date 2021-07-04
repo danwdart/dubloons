39bb9df6eb47388279d137f6d90120b536eb5171
Dan Dart <git@dandart.co.uk>
2021-07-04 13:33:16 +0100

    Perhaps slightly better ghc9 update

--- default-ghc9.nix
+++ default-ghc9.nix
@@ -4,37 +4,33 @@ let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
-      # TODO fix cryptonite to use integer-gmp 1.1
-      # https://github.com/haskell-crypto/cryptonite/issues/340
-      cryptonite = self.callHackage "cryptonite" "0.28" {};
-      # Earliest version supporting ghc 9.0.1
-      hxt = self.callHackage "hxt" "9.3.1.22" {};
-      # https://github.com/vincenthz/hs-memory/issues/84
-      memory = (self.callHackage "memory" "0.15.0" {}).overrideDerivation(self: {
-        prePatch = ''
-          sed -zi 's/import\(\s\+\)GHC.Prim/import\1GHC.Prim hiding (unsafeCoerce#)\nimport\1GHC.Exts (unsafeCoerce#)/' Data/ByteArray/Bytes.hs
-          sed -zi 's/import\(\s\+\)GHC.Prim/import\1GHC.Prim hiding (unsafeCoerce#)\nimport\1GHC.Exts (unsafeCoerce#)/' Data/ByteArray/ScrubbedBytes.hs
-        '';
-      });
-      # https://github.com/Soostone/retry/issues/71
-      retry = (self.callHackage "retry" "0.8.1.2" {}).overrideDerivation(self: {
-        prePatch = ''
-          sed -i 's/fmap \(.*\) genDuration/fmap (\\x -> \1 x) genDuration/g' test/Tests/Control/Retry.hs
-        '';
-      });
+      cryptonite = self.callCabal2nix "cryptonite" (nixpkgs.pkgs.fetchFromGitHub {
+        owner = "haskell-crypto";
+        repo = "cryptonite";
+        rev = "cryptonite-v0.29";
+        sha256 = "12nvrsrb1pdkif8nykiaby4sksry3kxq1j89z6x67h66232h5ril";
+      }) {};
+      # hxt = self.callHackage "hxt" "9.3.1.22" {};
+      memory = self.callCabal2nix "memory" (nixpkgs.pkgs.fetchFromGitHub {
+        owner = "vincenthz";
+        repo = "hs-memory";
+        rev = "memory-v0.16.0";
+        sha256 = "12nvrsrb1pdkif8nykiaby4sksry3kxq1j89z6x67h66232h5ril";
+      }) {};
+      # retry = self.callHackage "retry" "0.9.0.0" {};
       profunctors = self.callHackage "profunctors" "5.6.2" {};
-      # https://github.com/haskell-foundation/foundation/issues/551
-      foundation = (self.callHackage "foundation" "0.0.25" {}).overrideDerivation(self: {
-        prePatch = ''
-          sed -i 's/return . flip unConduit rest . onExc/\\x -> return \$ unConduit (onExc x) rest/g' Foundation/Conduit/Internal.hs
-        '';
-      });
-      # https://github.com/haskell-foundation/foundation/issues/548 ->
-      # https://github.com/haskell-foundation/foundation/pull/549
-      basement = (self.callCabal2nixWithOptions "basement" (builtins.fetchGit {
-        url = "https://github.com/LaurentRDC/foundation.git";
-        rev = "d02ab182c977272ae4089ce20d15d82fc4fcd5e8";
-      }) "--subpath basement" {});
+      foundation = self.callCabal2nixWithOptions "foundation" (nixpkgs.pkgs.fetchFromGitHub {
+        owner = "haskell-foundation";
+        repo = "foundation";
+        rev = "foundation-v0.0.26.1";
+        sha256 = "0a61c9b6zmcqy2phrz77ndjywsycxp3gz2k9m2vrzyg0gqj1a078";
+      }) "--subpath foundation" {};
+      basement = self.callCabal2nixWithOptions "basement" (nixpkgs.pkgs.fetchFromGitHub {
+        owner = "haskell-foundation";
+        repo = "foundation";
+        rev = "foundation-v0.0.26.1";
+        sha256 = "0a61c9b6zmcqy2phrz77ndjywsycxp3gz2k9m2vrzyg0gqj1a078";
+      }) "--subpath basement" {};
       dubloons = self.callCabal2nix "dubloons" (gitignore ./.) {};
     };
   };
@@ -50,7 +46,7 @@ let
       nixpkgs.haskellPackages.stylish-haskell
       nixpkgs.haskellPackages.hlint
     ];
-    # withHoogle = true;
+    withHoogle = true;
   };
   exe = nixpkgs.haskell.lib.justStaticExecutables (myHaskellPackages.dubloons);
 in
